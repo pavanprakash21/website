6e5d7c106170fc0aa0f0f0f745ab02fc71a43709
Pavan Prakash <pavan.prakash.contractor@move.com>
2019-07-05 08:56:54 +0530

    Create home page

---
+++ components/footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Link from './link';
+
+const StyledFooter = styled.footer`
+  flex-shrink: 0;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  padding: 30px 20px;
+  @media (min-width: 737px) {
+    margin: 0 auto;
+    max-width: 41em;
+    padding: 45px;
+  }
+`;
+
+export default () => (
+  <StyledFooter>
+    <Container>
+      <Link underline>{'pavanprakash.ml'}</Link>
+    </Container>
+  </StyledFooter>
+);
